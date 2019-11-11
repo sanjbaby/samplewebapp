c8cbc01c4130e0147bde16086eb621938765dcdc
sanjbaby <53223902+sanjbaby@users.noreply.github.com>
2019-11-11 22:01:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hahahah ana mndtharam hello world! This message is from sanju why not shown. Finallly I think I have done it"
+    return "Test from GIT! This message is from sanju why not shown. Finallly I think I have done it"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
