c5690ca87e9f61745743fd326ad888b4731e7e5b
sanjbaby <53223902+sanjbaby@users.noreply.github.com>
2019-11-12 10:35:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Test from GIT Good morning! This message is from sanju why not shown. Finallly I think I have done it"
+<input type="submit" name="submit_button" value="Do Something">
+<input type="submit" name="submit_button" value="Do Something Else">
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
