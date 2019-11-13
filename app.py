74e420ea9c5748502e0c5f396718ae40a0972e23
Sanju Baby <sanjbaby@in.ibm.com>
2019-11-13 15:05:51 +0530

    curl test

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask,render_template
-
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
-    return render_template('home.html')
-    curl -X POST -u "apikey:FIXTaEnWS9sySkJr91J8tiNXST1ZYpu-8U4a5QJfmU_K" --header "Content-Type: audio/flac" --data - binary @ C:\Data\Container_Crush\learning\audio - file.flac "https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize"
+
+    headers = {
+        'Content-Type': 'audio/flac',
+    }
+
+    data = open('C:\Data\Container_Crush\learning\audio-file.flac', 'rb').read()
+    response = requests.post('https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize', headers=headers,
+                             data=data, auth=('apikey', 'FIXTaEnWS9sySkJr91J8tiNXST1ZYpu-8U4a5QJfmU_K'))
+    return render_template('home.html',value=response)
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
