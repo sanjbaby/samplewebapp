1b8b78a6bf8d1f04ee0632b6abc9b4ee2de76ef4
Sanju Baby <sanjbaby@in.ibm.com>
2019-11-13 14:40:17 +0530

    curl test

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 
 def home():
     return render_template('home.html')
+    curl - X POST - u "apikey:FIXTaEnWS9sySkJr91J8tiNXST1ZYpu-8U4a5QJfmU_K" \
+    --header "Content-Type: audio/flac" \
+    --data - binary @ C:\Data\Container_Crush\learning\audio - file.flac \
+    "https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize"
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
