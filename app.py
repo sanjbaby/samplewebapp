8b84fe09312c91e9b9f16d8ff64754a1835728ca
sanjbaby <53223902+sanjbaby@users.noreply.github.com>
2019-12-05 22:06:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+
 from flask import Flask,render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
     url = 'https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize'
-    headers = {'Content-Type': 'audio/flac'}
+    #headers = {'Content-Type': 'audio/flac'}
     #data = open('C:\\Data\\Container_Crush\\learning\\audio-file.flac', 'rb').read()
-    #response = requests.post('https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize', headers=headers,
-                             #data=data, auth=('apikey', 'FIXTaEnWS9sySkJr91J8tiNXST1ZYpu-8U4a5QJfmU_K'))
-    return render_template('home.html',value="Sanju From normal git")
+    #response = requests.post('https://api.us-south.speech-to-text.test.watson.cloud.ibm.com/instances/b20b578f-60a7-4e72-a3b6-08f691ff9b3b/v1/recognize', headers=headers,
+     #                        data=data, auth=('apikey', 'TItad1CugTOubrsb2zXaOhswr7XB_RJvYRMn_E4hQDRO'))
+    #new_value=response.json()
+    #new_value=new_value['results']
+    return render_template('home.html',value="Sanju")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
