4553157ff9e26a827310038d03e053d252fcc7ff
sanjbaby <53223902+sanjbaby@users.noreply.github.com>
2019-12-05 16:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def home():
     url = 'https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize'
     headers = {'Content-Type': 'audio/flac'}
-    data = open('C:\\Data\\Container_Crush\\learning\\audio-file.flac', 'rb').read()
-    response = requests.post('https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize', headers=headers,
-                             data=data, auth=('apikey', 'FIXTaEnWS9sySkJr91J8tiNXST1ZYpu-8U4a5QJfmU_K'))
-    return render_template('home.html',value=response.json())
+    #data = open('C:\\Data\\Container_Crush\\learning\\audio-file.flac', 'rb').read()
+    #response = requests.post('https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize', headers=headers,
+                             #data=data, auth=('apikey', 'FIXTaEnWS9sySkJr91J8tiNXST1ZYpu-8U4a5QJfmU_K'))
+    return render_template('home.html',value="Sanju From normal git")
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
